1f63eb2d93bb6af2c0af56064b56e52d7ce851e9
Logobot3000 <117487576+Logobot3000@users.noreply.github.com>
2022-11-04 18:09:14 -0400

    Add files via upload

---
+++ js.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("mySidenav").style.width = "750px";
+  document.getElementById("main").style.marginRight = "750px";
+}
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+}
