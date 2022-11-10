4fd21c00d6a451e197bf132fa83474adc4e9a0e9
Logobot3000 <kroll.logan@gmail.com>
2022-11-09 21:12:18 -0500

    sticky navbar test 1 so it doesnt look terrible

---
+++ js.js
@@ -0,0 +1,11 @@
+
+window.onscroll = function() {stick()};
+var navbar = document.getElementById("navthing");
+var sticky = navbar.offsetTop;
+function stick() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
