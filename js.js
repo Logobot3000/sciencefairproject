5ca029631d1b08a94ea8cbb80c07b7d886a3ae5a
Logobot3000 <kroll.logan@gmail.com>
2022-11-09 21:03:16 -0500

    column and nav update

--- js.js
+++
@@ -1,8 +0,0 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "35%";
-  document.getElementById("main").style.marginRight = "35%";
-}
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
-  document.getElementById("main").style.marginRight = "0";
-}
